0a32e71bc68a4e74c43098ef81c733ec5a336367
dahaekim88 <dhkim1014@gmail.com>
2019-04-21 12:33:31 +0900

    Update app with Layout component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
-import "./App.css";
+import { createGlobalStyle } from "styled-components";
 
-class App extends Component {
-  render() {
-    return <div className="App">Hello, Asiance!</div>;
-  }
-}
+import Layout from "./Layout";
+
+const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Raleway:300,400,700,800|Staatliches');
+
+    * { 
+      box-sizing: border-box;
+      margin:0;
+      padding: 0; 
+    }
+
+    body {
+      font-family: "Raleway", sans-serif;
+      width: 100%;
+      height: 100%;
+      overflow-y: scroll;
+    }
+`;
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Layout>Hello, Asiance!</Layout>;
+    </>
+  );
+};
 
 export default App;
